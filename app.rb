d4c238d4f363a26c3f47a243f176ec8c395eab2b
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-01 05:12:21 +0000

    Fixed run error

--- app.rb
+++ app.rb
@@ -4,37 +4,23 @@ require "rest-client"
 require "http"
 require "json"
 
-get '/input_form' do
-  erb :input_form
+get '/' do
+  erb(:input_form)
 end
 
 post '/submit' do
-  user_input = params['user_input']
+  user_input = params.fetch("user_input")
 
   api_url = "https://api.funtranslations.com/translate/yoda.json"
+
   response = RestClient.post(api_url, {text: user_input})
 
-  "API Response: #{response.body}"
+  api_hash = response.body
+
+  contents_hash = api_hash.fetch("contents")
+
+  @yoda = parsed_data.fetch("translated")
+
+  # render a view template where I show the symbols
+  erb(:translation)
 end
-# get("/") do
-#   # build the API url, including the API key in the query string
-#   api_url = "https://api.funtranslations.com/translate/yoda.json"
-
-#   # use HTTP.get to retrieve the API information
-#   raw_data = HTTP.get(api_url)
-
-#   # convert the raw request to a string
-#   raw_data_string = raw_data.to_s
-
-#   # convert the string to JSON
-#   parsed_data = JSON.parse(raw_data_string)
-
-#   # get the symbols from the JSON
-#   translator_hash = parsed_data.fetch("contents")
-
-#   # get the translation from the hash
-#   yoda = translator_hash.fetch("translated")
-
-#   # render a view template where I show the symbols
-#   erb(:homepage)
-# end
